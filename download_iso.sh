154ab85961a2e44a66215ea394556ef939358b95
M2kar <m2kar.cn@gmail.com>
2019-11-20 21:33:21 +0800

    Update download_iso.sh

--- download_iso.sh
+++ download_iso.sh
@@ -2,6 +2,6 @@
 set -e 
 set -x
 pushd /mnt/sec-srv3/os-iso
-curl https://gist.githubusercontent.com/m2kar/dedb289abe744d09b1ad393313ff073b/raw/5ea4e8f2cee6696461122522d71c27ba0ac1ab3f/iso.list \
+curl https://raw.githubusercontent.com/m2kar/iso_mirror/master/iso.list \
 | xargs -n2 -P4 -I {} bash -c "wget -c -t 10 -N -o wget.log -P {}"
 popd
